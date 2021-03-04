6458b5f174f9c2ca7f1bdc5ca0eeed7225ef3c8a
govido <12106056+govido@users.noreply.github.com>
2021-03-04 17:11:52 +0100

    fix PXE_skip path

--- MDTp installer/DeploymentShare/Scripts/MDTp/TS_Install_PXE_Boot_Skip.ps1
+++ MDTp installer/DeploymentShare/Scripts/MDTp/TS_Install_PXE_Boot_Skip.ps1
@@ -8,4 +8,4 @@ $pxegroup="PXE_SKIP"
 
 $report = @()
 $report += New-Object psobject -Property @{Date=$today;Hostname=$tsenv.Value("OSDComputername");ClientID=$tsenv.Value("MACADDRESS001");pxeparameter1=$pxebootprogram;pxeparameter2=$pxeextraparameter;pxeparameter3=$pxegroup}
-$report | select Date,Hostname,ClientID,pxeparameter1,pxeparameter2,pxeparameter3 | export-csv "\\mdtp\DeploymentShare$\Scripts\SIT\Serverscripts\WDS_PXE\$filename.csv" -NoTypeInformation
+$report | select Date,Hostname,ClientID,pxeparameter1,pxeparameter2,pxeparameter3 | export-csv "\\mdtp\DeploymentShare$\Scripts\MDTp\\Servertasks\WDS_PXE\$filename.csv" -NoTypeInformation
