06f7cb5f6bcfbfdc6cebea47c8d81bd0e84e6fe5
govido <12106056+govido@users.noreply.github.com>
2021-03-31 13:27:58 +0200

    add local user accounts with TS variables

---
+++ MDTp installer/DeploymentShare/Scripts/MDTp/TS_Install_Local_Users.ps1
@@ -0,0 +1,24 @@
+# Get the TS variables
+$tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+$localusers = $tsenv.Value("localusers")
+$localpasswords = $tsenv.Value("localpasswords")
+$localadmins = $tsenv.Value("localadmins")
+$localadminpasswords = $tsenv.Value("localadminpasswords")
+
+$localuserlist=$localusers.split(";")
+$localpasswordlist=$localpasswords.split(";")
+for ($i=0; $i -lt $localuserlist.length; $i++)
+{
+    New-LocalUser -Name $userlist[$i] -Password (ConvertTo-SecureString $localpasswordlist[$i] -AsPlainText -Force) -Description "local User-Account"
+    #normal Users Group
+    Add-LocalGroupMember -SID "S-1-5-32-545" -Member $localuserlist[$i]
+    }
+
+$localadminlist=$localadmins.split(";")
+$localadminpasswordlist=$localadminpasswords.split(";")
+for ($i=0; $i -lt $localadminlist.length; $i++)
+{
+    New-LocalUser -Name $localadminlist[$i] -Password (ConvertTo-SecureString $localadminpasswordlist[$i] -AsPlainText -Force) -Description "local Admin-Account"
+    #Administrators Group
+    Add-LocalGroupMember -SID "S-1-5-32-544" -Member $localadminlist[$i]
+    }
