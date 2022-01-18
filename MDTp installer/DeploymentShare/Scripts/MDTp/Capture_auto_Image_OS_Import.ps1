381c06f0cc16c25eef88ffd9baabe3aa73c5d754
govido <12106056+govido@users.noreply.github.com>
2022-01-18 13:30:57 +0100

    Potential auto-import fix

--- MDTp installer/DeploymentShare/Scripts/MDTp/Capture_auto_Image_OS_Import.ps1
+++ MDTp installer/DeploymentShare/Scripts/MDTp/Capture_auto_Image_OS_Import.ps1
@@ -1,4 +1,4 @@
-#Creat file with imagename and path information for autoimport after capture, servertasks checks every 5 minutes for finished captures to import 
+#Create file with imagename and path information for autoimport after capture, servertasks checks every 5 minutes for finished captures to import 
 $tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
 $today = Get-Date -Format yyyy_MM_dd_HH_mm_ss
 
@@ -8,4 +8,4 @@ $description = $tsenv.Value("Description")
 
 $report = @()
 $report += New-Object psobject -Property @{Date=$today;Imagepath=$imagepath;Foldername=$foldername;Description=$description}
-$report | Select-Object Date,Imagepath,Foldername,Description | export-csv \\mdtp\DeploymentShare\Scripts\MDTp\Servertasks\OS_Import\import_os_$foldername.csv -NoTypeInformation -append
+$report | Select-Object Date,Imagepath,Foldername,Description | export-csv \\mdtp\DeploymentShare$\Scripts\MDTp\Servertasks\OS_Import\import_os_$foldername.csv -NoTypeInformation -append
